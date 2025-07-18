d265c7568c541297f98005f7db1ad4497fa133fc
anirudhgv1805 <anirudhgv04@gmail.com>
2025-07-18 14:13:44 +0530

    added: re-route to login on expired token

--- frontend-react/src/utils/axiosInstance.ts
+++ frontend-react/src/utils/axiosInstance.ts
@@ -1,43 +1,48 @@
-import axios from 'axios';
-import { useEffect } from 'react';
+import axios from "axios";
+import { useEffect } from "react";
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,  
-    headers: {
-        'Content-Type': 'application/json',
-    },
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+  },
 });
 
 export const useAxiosInstance = () => {
-    const  jwtToken = localStorage.getItem("jwtToken");
+  const jwtToken = localStorage.getItem("jwtToken");
 
-    useEffect(() => {
-        const requestInterceptor = axiosInstance.interceptors.request.use(
-            (config) => {
-                if (jwtToken) {
-                    config.headers['Authorization'] = `Bearer ${jwtToken}`;
-                }
-                return config;
-            },
-            (error) => Promise.reject(error)
-        );
+  useEffect(() => {
+    const requestInterceptor = axiosInstance.interceptors.request.use(
+      (config) => {
+        if (jwtToken) {
+          config.headers["Authorization"] = `Bearer ${jwtToken}`;
+        }
+        return config;
+      },
+      (error) => Promise.reject(error)
+    );
 
-        const responseInterceptor = axiosInstance.interceptors.response.use(
-            (response) => response,
-            (error) => {
-                if (error.response?.status === 401) {
-                    console.log('Unauthorized access, redirecting to login...');
-                    window.location.href = '/login';
-                }
-                return Promise.reject(error);
-            }
-        );
+    const responseInterceptor = axiosInstance.interceptors.response.use(
+      (response) => response,
+      (error) => {
+        if (error.response?.status === 401) {
+          console.log("Unauthorized access, redirecting to login...");
+          window.location.href = "/";
+        }
+        if (error.response?.status === 500) {
+          console.log("Token Expired Redirecting to Login page");
+          localStorage.removeItem("jwtToken");
+          window.location.href = "/";
+        }
+        return Promise.reject(error);
+      }
+    );
 
-        return () => {
-            axiosInstance.interceptors.request.eject(requestInterceptor);
-            axiosInstance.interceptors.response.eject(responseInterceptor);
-        };
-    }, [jwtToken]);
+    return () => {
+      axiosInstance.interceptors.request.eject(requestInterceptor);
+      axiosInstance.interceptors.response.eject(responseInterceptor);
+    };
+  }, [jwtToken]);
 
-    return axiosInstance;
+  return axiosInstance;
 };
